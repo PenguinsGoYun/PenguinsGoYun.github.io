5f0f34dd96ba2ca9de011193b8361adda237b813
PenguinsGoYun <yeonjunkim99@gmail.com>
2019-11-15 12:58:30 -0500

    Updates

---
+++ precache-manifest.b56fc051a9877486b612c21efd91d356.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8e89532e77c9bd57bc1622b4ecfe576",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec83cb63b3861daabcb1",
+    "url": "/static/css/main.afd0b80e.chunk.css"
+  },
+  {
+    "revision": "45e27bc5b63307e4a9c0",
+    "url": "/static/js/2.9e7a02e5.chunk.js"
+  },
+  {
+    "revision": "ec83cb63b3861daabcb1",
+    "url": "/static/js/main.9bc3849c.chunk.js"
+  },
+  {
+    "revision": "c3119f27f15aa4d06ce6",
+    "url": "/static/js/runtime-main.a82f5602.js"
+  }
+]);
